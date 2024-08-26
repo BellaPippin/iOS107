a36e2aea15f508d6184b5f9700bda503e822ba58
Maria Cosenza <mbcosenza@hotmail.com>
2024-08-26 14:05:29 -0500

    Hello World

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+//Practicing using the print function
+
+print("Hello World")
+
+let name = "Bill"
+let language = "Swift"
+
+print ("Welcome \(name), to the wonderful world of \(language)!")
